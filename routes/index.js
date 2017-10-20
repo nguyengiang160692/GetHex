f3979554549940cf511c72a65ff5886bfe6152a2
Nguyen Giang <giang.nguyen@alta.com.vn>
2017-10-20 17:45:47 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express    = require('express');
 var Decaptcher = require("de-captcher").decaptcher;
 var router     = express.Router();
+var decaptcher = new Decaptcher("nhantruong", "32541234");
 /* GET home page. */
 router.get('/', function(req, res, next){
     res.render('index', {title: 'Express'});
 });
 
 router.get('/capcha', function(req, res, next){
-    var decaptcher = new Decaptcher("nhantruong", "32541234");
+    
     decaptcher.postPicture(req.query.url, "image/png", function(err, result){
         res.send(result.Text);
     });
