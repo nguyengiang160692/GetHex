915771b1ced166074d0b0d1423bf0b3768eb13d2
unknown <nguyengiang160692@gmail.com>
2017-10-22 01:02:02 +0700

    UPDATE

---
+++ public/auto.js
@@ -0,0 +1,42 @@
+function getLastInputWithTypeInZone(_inputs, _type){
+    var last = null;
+    for(var x = 0; x < _inputs.length; x++){
+        var child = _inputs[x];
+        if(child.type == _type){
+            last = child;
+        }
+    }
+    return last;
+}
+
+var api_key = "PDMXB4543888A";
+
+var inputs         = document.getElementById('ico-form')
+    .querySelectorAll("input, select, checkbox, textarea");
+var imgs           = document.getElementById('ico-form')
+    .querySelectorAll("img");
+var input_amount   = document.querySelector('[data-fill="#hxt_amount"]');
+var capcha_input   = getLastInputWithTypeInZone(inputs, 'text');
+capcha_input.value = '';
+var submited       = false;
+setInterval(function(){
+    input_amount.click();
+    capcha_input.focus();
+    
+    if(capcha_input.value.length == 4 && submited == false){
+        document.forms["ico-form"].submit();
+        submited = !submited;
+        window.open(window.location.href, '_blank');
+    }
+}, 100);
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://apipro1.ocr.space/parse/imageurl?apikey=" + api_key + "&url=" + encodeURI(imgs[0].currentSrc), true);
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status == 200 && capcha_input.value == ''){
+        var capcha         = JSON.parse(xhr.responseText).ParsedResults[0].ParsedText.trim();
+        capcha_input.value = capcha;
+    }
+};
+xhr.send();
+
