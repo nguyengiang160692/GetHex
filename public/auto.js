09e3b1658191c565d60dba1b92bb2506983d7ec2
unknown <nguyengiang160692@gmail.com>
2017-10-23 22:33:15 +0700

    UPDATE

--- public/auto.js
+++ public/auto.js
@@ -29,7 +29,15 @@ setInterval(function(){
         window.open(window.location.href, '_blank');
     }
 }, 100);
-
+setInterval(function(){
+    var hour           = document.getElementById('ico-in-hh').innerHTML;
+    var minutes        = document.getElementById('ico-in-mm').innerHTML;
+    var seconds        = document.getElementById('ico-in-ss').innerHTML;
+    var password_input = document.getElementById('password');
+    if(password_input == null && (hour == '00' || hour == '23') && (minutes == '00' || minutes == '59') && (parseInt(seconds) < 2 || parseInt(seconds) > 45)){
+        window.location.reload();
+    }
+}, 1000);
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://apipro1.ocr.space/parse/imageurl?apikey=" + api_key + "&url=" + encodeURI(imgs[0].currentSrc), true);
 xhr.onreadystatechange = function(){
