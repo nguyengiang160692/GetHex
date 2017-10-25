545abb3415cf0bc0087e557f501b77407573376f
unknown <nguyengiang160692@gmail.com>
2017-10-26 00:39:21 +0700

    UPDATE

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express    = require('express');
 var Decaptcher = require("de-captcher").decaptcher;
+var Tesseract  = require('tesseract.js')
+var request    = require('request')
+var fs         = require('fs')
+
 var router     = express.Router();
 var decaptcher = new Decaptcher("nhantruong", "32541234");
 /* GET home page. */
@@ -8,10 +12,26 @@ router.get('/', function(req, res, next){
 });
 
 router.get('/capcha', function(req, res, next){
-    
     decaptcher.postPicture(req.query.url, "image/png", function(err, result){
         res.send(result.Text);
     });
 });
 
+router.get('/recap', function(req, res, next){
+    var filename        = Date.now() + '.png';
+    var writeFileStream = fs.createWriteStream(`./images/${filename}`);
+    request(req.query.url)
+        .pipe(writeFileStream)
+        .on('close', function(){
+            console.log(req.query.url, 'saved to', filename)
+            Tesseract.recognize(filename)
+                .then(function(result){
+                    console.log(result.text)
+                    res.send(result.text);
+                })
+            
+        })
+})
+
 module.exports = router;
+
